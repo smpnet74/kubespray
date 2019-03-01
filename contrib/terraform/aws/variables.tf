d516a7041f922dbd69f0ebbb2662b596b2fd82ef
Scott Peterson <sm_peterson@yahoo.com>
2019-03-01 12:42:09 -0600

    changing the vars file

--- contrib/terraform/aws/variables.tf
+++ contrib/terraform/aws/variables.tf
@@ -1,10 +1,10 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key"
-}
+#variable "AWS_ACCESS_KEY_ID" {
+#  description = "AWS Access Key"
+#}
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Key"
-}
+#variable "AWS_SECRET_ACCESS_KEY" {
+#  description = "AWS Secret Key"
+#}
 
 variable "AWS_SSH_KEY_NAME" {
   description = "Name of the SSH keypair to use in AWS."
